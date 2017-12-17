de9009ccd85ce864f0014394e513d73c4b2a9277
sofiamgsfernandes <sofiamgsfernandes@gmail.com>
2017-12-17 19:37:30 +0000

    prover

--- prover.py
+++ prover.py
@@ -1,8 +1,32 @@
 """ A program to convert logical sentences in propositional logic into the
 clausal normal form (CNF)."""
-
+from resolution_util import *
 import sys, fileinput
+
+
+graph = Graph()
 
 #read file from stdin line by line
 for line in fileinput.input():
-    print(line)
+	line = line.replace(' ', '')
+	clause = line[0:-1]
+	graph.add_vertex(clause)
+
+problem = Resolution(graph.vertices, graph.num_vertices)
+
+for i in graph.vertices:
+	if len(eval(i)) == 1:
+		problem.knowledge_base.insert(0,i)
+	else:
+		problem.knowledge_base.append(i)
+
+# Resolution result
+result = problem.resolution_algorithm()
+
+if result == True:
+	print("True")
+
+if result == False:
+	print("No solution")
+
+sys.exit(0)
